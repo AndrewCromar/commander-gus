32d421e6bfabe3e33ad134639bd4d764da41ff97
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-06-03 20:48:01 -0700

    add reset code #4

--- godot-project/scripts/entities/player.gd
+++ godot-project/scripts/entities/player.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 func _ready() -> void:
+	Global.reset()
+	
 	area_entered.connect(on_block)
 
 func on_block(area: Area2D) -> void:
--- godot-project/scripts/global.gd
+++ godot-project/scripts/global.gd
@@ -7,4 +7,11 @@ var HEALTH : int = starting_health
 
 var CAMERA_SHAKE_DURATION : float
 
-var FIRE_RATE : float = 1
+var starting_fire_rate : float = 1
+var FIRE_RATE : float = starting_fire_rate
+
+func reset() -> void:
+	SCORE = 0
+	CAMERA_SHAKE_DURATION = 0
+	HEALTH = starting_health
+	FIRE_RATE = starting_fire_rate
